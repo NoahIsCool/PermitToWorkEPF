522921a6a55a466c36e315a96b9a3d0995348af8
Zane Gray <zgray@lan.snomnh.ou.edu>
2020-02-06 14:30:40 -0600

    topnav alignment and branding

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -27,7 +27,9 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="topnav navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">tmp</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">
+		    <img src="logo.png" alt="" width="30" height="30"/> 
+	    </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
