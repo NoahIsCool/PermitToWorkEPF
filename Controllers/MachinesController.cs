3fdc1241141e6160262a2c615f91427ba43b8732
Gray <zane.j.gray@ou.edu>
2020-03-10 11:36:57 -0500

    added crud operations for all datamodels and dbcontexts

---
+++ Controllers/MachinesController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PermitToWorkRepf.Data;
+using PermitToWorkRepf.Models;
+
+namespace tmp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachinesController : ControllerBase
+    {
+        private readonly RepfDbContext _context;
+
+        public MachinesController(RepfDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Machines
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Machine>>> GetMachines()
+        {
+            return await _context.Machines.ToListAsync();
+        }
+
+        // GET: api/Machines/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Machine>> GetMachine(string id)
+        {
+            var machine = await _context.Machines.FindAsync(id);
+
+            if (machine == null)
+            {
+                return NotFound();
+            }
+
+            return machine;
+        }
+
+        // PUT: api/Machines/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMachine(string id, Machine machine)
+        {
+            if (id != machine.name)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(machine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MachineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Machines
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Machine>> PostMachine(Machine machine)
+        {
+            _context.Machines.Add(machine);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MachineExists(machine.name))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetMachine", new { id = machine.name }, machine);
+        }
+
+        // DELETE: api/Machines/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Machine>> DeleteMachine(string id)
+        {
+            var machine = await _context.Machines.FindAsync(id);
+            if (machine == null)
+            {
+                return NotFound();
+            }
+
+            _context.Machines.Remove(machine);
+            await _context.SaveChangesAsync();
+
+            return machine;
+        }
+
+        private bool MachineExists(string id)
+        {
+            return _context.Machines.Any(e => e.name == id);
+        }
+    }
+}
---
+++ Migrations/20200310161214_REPF.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tmp.Migrations
+{
+    public partial class REPF : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Machines",
+                columns: table => new
+                {
+                    name = table.Column<string>(nullable: false),
+                    notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machines", x => x.name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SignIns",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    time = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SignIns", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<ulong>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Team = table.Column<string>(nullable: true),
+                    Graduation = table.Column<DateTime>(nullable: false),
+                    SignInStudentId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_SignIns_SignInStudentId",
+                        column: x => x.SignInStudentId,
+                        principalTable: "SignIns",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SignInStudentId",
+                table: "Students",
+                column: "SignInStudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Machines");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "SignIns");
+        }
+    }
+}
