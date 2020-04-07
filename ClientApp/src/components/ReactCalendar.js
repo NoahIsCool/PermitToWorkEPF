1ec2ae1a99de116cd68f9a935522b9ed9907cf97
Skylar Smith <skylarsmith@Skylars-MacBook-Air.local>
2020-04-07 15:59:15 -0500

    Fixing branches

--- ClientApp/src/components/ReactCalendar.js
+++ ClientApp/src/components/ReactCalendar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -50,62 +49,3 @@ export class ReactCalendar extends Component {
         );
     }
 }
-=======
-import React from 'react'
-import { Views } from 'react-calendar/src'
-import { Calendar } from 'react-calendar'
-import { events, ExampleControlSlot } from 'react-calendar/examples'
-
-const propTypes = {}
-
-export class ReactCalendar extends React.Component {
-  constructor(...args) {
-    super(...args)
-
-    this.state = { events }
-  }
-
-  handleSelect = ({ start, end }) => {
-    const title = window.prompt('New Event name')
-    if (title)
-      this.setState({
-        events: [
-          ...this.state.events,
-          {
-            start,
-            end,
-            title,
-          },
-        ],
-      })
-  }
-
-  render() {
-    const { localizer } = this.props
-    return (
-      <>
-        <ExampleControlSlot.Entry waitForOutlet>
-          <strong>
-            Click an event to see more info, or drag the mouse over the calendar
-            to select a date/time range.
-          </strong>
-        </ExampleControlSlot.Entry>
-        <Calendar
-          selectable
-          localizer={localizer}
-          events={this.state.events}
-          defaultView={Views.WEEK}
-          scrollToTime={new Date(1970, 1, 1, 6)}
-          defaultDate={new Date(2015, 3, 12)}
-          onSelectEvent={event => alert(event.title)}
-          onSelectSlot={this.handleSelect}
-        />
-      </>
-    )
-  }
-}
-
-ReactCalendar.propTypes = propTypes
-
-export default ReactCalendar
->>>>>>> b1340b584e01fc7a5d978e5f2918d7d7427a80ac
